723b9919cff40a88a1a2cccb45141a786afd63d2
EgorKirillov <80296253508@mail.ru>
2022-10-21 19:26:11 +0300

    refactor: change data view

--- src/features/table/UI/tableBody/row.tsx
+++ src/features/table/UI/tableBody/row.tsx
@@ -2,15 +2,22 @@ import React from 'react';
 import {ItemType} from "../../itemsReducer";
 
 type RowType = {
-  // order: number
   item:ItemType
 }
 
 export const Row = ({item}:RowType) => {
+  
+  //преобразование отображения даты
+  const date = new Date(item.date).toLocaleString('ru', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
   return (
     <tr>
       <td>{item.name}</td>
-      <td>{item.date}</td>
+      <td>{date}</td>
       <td>{item.count}</td>
       <td>{item.distance}</td>
     </tr>
