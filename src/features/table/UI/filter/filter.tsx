d7ea9134c2aa7533b068a6cc25ac7312e0d12233
EgorKirillov <80296253508@mail.ru>
2022-10-21 19:23:08 +0300

    add: add clear filter function, add comments, clear code

--- src/features/table/UI/filter/filter.tsx
+++ src/features/table/UI/filter/filter.tsx
@@ -1,86 +1,113 @@
-import React, {ChangeEvent, SyntheticEvent, useState} from 'react';
-import {ColumnSortNames, changeQueryParams, ColumnFilterNames} from "../../itemsReducer";
+import React, {ChangeEvent, FormEvent, SyntheticEvent, useState} from 'react';
+
+import {ColumnSortNames, changeQueryParams, ColumnFilterNames, clearFilter} from "../../itemsReducer";
 import {useAppDispatch} from "../../../../common/hooks/hooks";
 import {createFilterQueryParams} from "../../../../common/utils/createQueryParams";
+
 import styles from './filter.module.css'
 
 export type MethodType = 'none' | '=' | 'included' | '>' | '<'
 
-enum FilterNamesEnum {'name' = 1, 'count', 'distance', 'date'}
-
-enum FilterMethodEnum {'none', '=', 'included', '>', '<'}
-
 export const Filter = () => {
-  const [name, setName] = useState<ColumnFilterNames | undefined>(undefined)
+  
+  const [filterName, setFilterName] = useState<ColumnFilterNames | 'none'>('none')
   const [method, setMethod] = useState<MethodType>("none")
   const [filterValue, setFilterValue] = useState("")
   const [timerId, setTimerId] = useState(0)
   
   const dispatch = useAppDispatch()
   
-  const filterNames: ColumnFilterNames[] = ['name', 'count', 'distance', 'date']
-  let filterMethod: MethodType[]
-  if (name !=='name') {
-    filterMethod = ['none', '=', '>', '<']
-  } else {filterMethod= ['none', '=', 'included']}
+  const filterNames: (ColumnFilterNames | 'none')[] = ['none', 'name', 'count', 'distance', 'date']
+  
+  //определение методов фильтрации в зависимости от сортируемого столбца
+  let filterMethods: MethodType[]
+  if (filterName !== 'name') {
+    filterMethods = ['none', '=', '>', '<']
+  } else {
+    filterMethods = ['none', '=', 'included']
+  }
   
   
   // выбор имени колонки по которой производится фильтрация
   const selectName = (e: SyntheticEvent<HTMLSelectElement, Event>) => {
-    let index = e.currentTarget.options.selectedIndex
-    setName(FilterNamesEnum[index] as ColumnSortNames)
+    //обнуляем  метода и значения фильтрации если они не 'пустые' (изменение столбца фильтрации)
+    if (method !== "none") setMethod("none")
+    if (filterValue !== "") setFilterValue("")
+    
+    setFilterName(e.currentTarget.value as ColumnSortNames)
   }
   
   //выбор способа фильтрации
   const selectMethod = (e: SyntheticEvent<HTMLSelectElement, Event>) => {
-    let index = e.currentTarget.options.selectedIndex
-    // console.log(FilterNamesEnum[index])
-    console.log(FilterMethodEnum[index])
-    setMethod(FilterMethodEnum[index] as MethodType)
+    //обнуляем значение фильтрации если оно не 'пустое' (изменение метода фильтрации)
+    setFilterValue('')
+    setMethod(e.currentTarget.value as MethodType)
   }
   
-  //ввод значения фильтрации и отправка в стэйт данных через 1с после остановки ввода
+  //ввод значения фильтрации и отправка в стэйт данных через 1с после остановки ввода debounсing
   const changeInput = (e: ChangeEvent<HTMLInputElement>) => {
     setFilterValue(e.currentTarget.value)
     const val = e.currentTarget.value
     //если происходит ввод данных - очистить предыдущий таймер
     clearTimeout(timerId)
     const id = +setTimeout(() => {
-      const filterParam = name ? createFilterQueryParams(name, method, val) : {}
-      console.log(filterParam)
+      const filterParam = filterName !== 'none' ? createFilterQueryParams(filterName, method, val) : {}
       dispatch(changeQueryParams(filterParam))
     }, 1000)
-    
     setTimerId(id)
   }
   
+  //очистка фильтрации
+  const clearFilterHandler = (e: FormEvent) => {
+    e.preventDefault()
+    setFilterName('none')
+    setMethod('none')
+    setFilterValue("")
+    dispatch(clearFilter())
+  }
+  
+  //отпределение типа input для ввода значений фильтрации
   const inputType =
-    name === 'name'
+    filterName === 'name'
       ? 'text'
-      : name === 'date'
+      : filterName === 'date'
         ? 'date'
         : 'number'
   
+  
   return (
-    <div>
       <form className={styles.filterForm}>
-        <div className={styles.filterColumn}><span>SELECT filter column</span>
-          <select name="column" placeholder={'filter by column'} value={undefined} onChange={selectName}>
-            <option value={"none"}> </option>
-            {filterNames.map(name => <option value={name}>{name.toUpperCase()}</option>)}
-          </ select></div>
+        <div>
+          <span>SELECT filter column</span>
+          <select name="column" placeholder={'filter by column'} value={filterName} onChange={selectName}>
+            
+            {filterNames.map(name =>
+              <option value={name} key={name}>{name.toUpperCase()}</option>)
+            }
+          </ select>
+        </div>
         
-        {name!! && < div className={styles.filterMethod}>< span>method</span>
-            <select name="method" placeholder={'filter by column'} onChange={selectMethod}>
-              {filterMethod.map(name => <option value={name}>{name.toUpperCase()}</option>)}
-            </ select></div>}
+        {/*отрисовка только если выбрана колонка фильтрации*/}
+        {filterName !== 'none' &&
+            <div>
+                <span>method</span>
+                <select name="method" placeholder={'filter by column'} onChange={selectMethod}>
+                  {filterMethods.map(filterMethod =>
+                    <option value={filterMethod} key={filterMethod}>{filterMethod.toUpperCase()}</option>)
+                  }
+                </ select>
+            </div>}
         
-        {name!! && method !== 'none' &&
-            <div className={styles.filterValue}><span>value</span>
+        {/*отрисовка только если выбрана колонка фильтрации и метод фильтрации*/}
+        {filterName !== 'none' && method !== 'none' &&
+            <div>
+                <span>value</span>
                 <input type={inputType} onChange={changeInput} value={filterValue}/>
             </div>}
+        
+        <button type={"submit"} onClick={clearFilterHandler}>clear filter</button>
+        
       </form>
-    </div>
   );
 };
 
